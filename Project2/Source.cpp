6d56c7e9447f721d0b37db1dc7e6bfcc13e40d54
Assay555 <jakob@mickenware.se>
2017-03-30 20:24:19 +0200

    String input fixad

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -5,55 +5,53 @@ using namespace std;
 int main()
 {
 	cout << "Hur har din dag varit idag mellan 1-10?" << endl;
-	int input;
+	string input;
 	int svar = 1;
 	cin >> input;
 	while (true){
-		if (input == 10) {
+		if (input == "10") {
 			cout << "Vad roligt!" << endl;
 			break;
 		}
-		else if (input == 9) {
+		else if (input == "9") {
 			cout << "Trevligt, trevligt" << endl;
 			break;
 		}
-		else if (input == 8) {
+		else if (input == "8") {
 			cout << "" << endl;
 			break;
 		}
-		else if (input == 7) {
+		else if (input == "7") {
 			cout << "" << endl;
 			break;
 		}
-		else if (input == 6) {
+		else if (input == "6") {
 			cout << "" << endl;
 			break;
 		}
-		else if (input == 5) {
+		else if (input == "5") {
 			cout << "" << endl;
 			break;
 		}
-		else if (input == 4) {
+		else if (input == "4") {
 			cout << "" << endl;
 			break;
 		}
-		else if (input == 3) {
+		else if (input == "3") {
 			cout << "" << endl;
 			break;
 		}
-		else if (input == 2) {
+		else if (input == "2") {
 			cout << "" << endl;
 			break;
 		}
-		else if (input == 1) {
+		else if (input == "1") {
 			cout << "KRAAAAAM!!!" << endl;
 			break;
 		}
 		else {
 			cout << "Jag sa mellan 1-10" << endl;
-			input = 0;
 			cin >> input;
-			system("pause");
 		}
 	}
 	system("pause");
